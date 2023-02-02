6ee8c7aac8d0ad822e9312a1fdddb4fc68c76324
Keivy Mora <mkeivy.mora@gmail.com>
2023-02-02 14:41:12 -0400

    initializing the project with websockets

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(app.get("port"), () => {
+  console.log("server on port", app.get("port"));
+});
+
+const SocketIO = require("socket.io")(server);
+const io = SocketIO.listen(server);
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+});
